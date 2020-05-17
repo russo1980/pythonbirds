ace3eb7c71f8ecff9da535814ba1f35c488a477f
russo1980 <cristianorusso@hotmail.com>
2020-05-17 16:13:53 -0300

    Criado e removido atributo dinâmico do tipo Pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -17,3 +17,8 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Tito'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(cristiano.__dict__)
+

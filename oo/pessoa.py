f84a633ecf8cf1bb187648db5245abaca3c54c19
russo1980 <cristianorusso@hotmail.com>
2020-05-17 16:36:38 -0300

    Criado método estatico por decorador e método de atributo de classe por decorador.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
     cristiano = Pessoa(nome='Cristiano')
     luciano = Pessoa(cristiano, nome='Luciano')
@@ -30,4 +38,7 @@ if __name__ == '__main__':
     print(luciano.olhos)
     print(cristiano.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(cristiano.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+
 
